c75c8fb245097a4ab54972ceda5b427f9f057111
abhisheknishad23 <iabhisheku4@gmail.com>
2026-07-05 21:27:05 +0530

    request body

--- request_body/Post_request.py
+++ request_body/Post_request.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+#simple formate
 @app.post('/create_user')
 def create_user(name:str, age:int):
     return{
         "name":name,
         "age":age
+    }
+#validation using pydantic
+class User(BaseModel):
+    name:str
+    age:int
+#json formate
+@app.post("/create-user")
+def create(user:User):
+    return{
+        "message":"new user created",
+        "data":user
     }
