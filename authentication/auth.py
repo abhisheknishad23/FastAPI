0c6155823cc92bc9a72e7cc7df4d3519a3e2f496
abhisheknishad23 <iabhisheku4@gmail.com>
2026-07-14 21:07:00 +0530

    auth

--- authentication/auth.py
+++ authentication/auth.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException, Depends, Header
+from pydantic import BaseModel
 from jose import jwt 
 from datetime import datetime, timedelta, timezone
 
@@ -17,3 +18,38 @@ def create_token(data: dict):
     token = jwt.encode(to_encode, seceret_key,algorithm=algo)
 
     return token
+
+#login api
+@app.post("/login")
+def login(username:str, password:str):
+    if username!= "admin" or password!="123":
+        raise HTTPException(
+            status_code=401,
+            detail="invalid username and password"
+        )
+    token = create_token({
+        "sub":username
+    })
+    return{
+        "success token": token
+    }
+
+#token varify
+def varify_token(token: str = Header(None)):
+    try:
+        payload = jwt.decode(token,seceret_key, algorithms=[algo])
+        return payload
+    except:
+        raise HTTPException(
+            status_code=401,
+            detail="InvLID OR EXPIRE TOKEN"
+        )
+    
+
+#protectd route
+@app.get("/secure")
+def secure_data(user = Depends(varify_token)):
+    return{
+        "message":"Secure Data Access",
+        "user":user
+    }
